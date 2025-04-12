93edfc413ed9a62914f273e2fb3f2fefe237c3ab
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-04-12 05:35:50 -0400

    added code to handle unknown gets

--- main.exs
+++ main.exs
@@ -12,13 +12,38 @@ defmodule Server do
     {:ok, client} = :gen_tcp.accept(socket)
 
     {:ok, request} = :gen_tcp.recv(client, 0)
-    IO.puts("Received request:\n#{request}")
 
-    response = """
-    HTTP/1.1 200 OK\r\n\r\n
-    """
+    request_str = to_string(request)
 
-    :gen_tcp.send(client, response)
+    s = String.split(request_str, ["\r\n"])
+
+    [get_request, host, user_agent, accept] = s
+    [request_type, request_route, http_version] = String.split(get_request, [" "])
+    IO.puts(host)
+    IO.puts(user_agent)
+    IO.puts(accept)
+
+
+    success_message = "HTTP/1.1 200 OK\r\n\r\n"
+    fail_message = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+    if request_route == "/" do
+      :gen_tcp.send(client, success_message)
+    else
+      :gen_tcp.send(client, fail_message)
+    end
+
+    # responseOK = """
+    # HTTP/1.1 200 OK\r\n\r\n
+    # """
+
+    # responseNotFound = """
+    # HTTP/1.1 404 Not Found\r\n\r\n
+    # """
+
+
+
+    # :gen_tcp.send(client, response)
     :gen_tcp.close(client)
   end
 end
