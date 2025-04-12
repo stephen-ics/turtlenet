5e4875b391adb2e4de53ec538190ff3683fee921
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-04-12 01:18:33 -0400

    send a 200 response back when connected

--- main.exs
+++ main.exs
@@ -6,11 +6,8 @@ defmodule Server do
   end
 
   def listen() do
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     IO.puts("Logs from your program will appear here!")
 
-    # Since the tester restarts your program quite often, setting SO_REUSEADDR
-    # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
     {:ok, client} = :gen_tcp.accept(socket)
 
@@ -18,11 +15,7 @@ defmodule Server do
     IO.puts("Received request:\n#{request}")
 
     response = """
-    HTTP/1.1 200 OK\r
-    Content-Type: text/plain\r
-    Content-Length: 13\r
-    \r
-    Hello, world!
+    HTTP/1.1 200 OK\r\n\r\n
     """
 
     :gen_tcp.send(client, response)
